80e42f71935898fedb19bb693065b5a9712c05b8
Gabriel Araujo <gabrielaraujomonteiro12@gmail.com>
2024-10-02 15:07:48 -0300

    Atividade 1

---
+++ RoletaRussa/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Random random = new Random();
+int numeroSorteado = random.Next(1, 7);
+Console.WriteLine("Insira um numero de 1 a 6: ");
+if (int.TryParse(Console.ReadLine(), out int numeroEscolhido))
+{
+    Console.WriteLine($"O revólver caiu em: {numeroSorteado}");
+ if (numeroEscolhido == numeroSorteado)
+ {
+    Console.WriteLine("Você morreu!");
+ }
+
+ else 
+ {
+    Console.WriteLine("Você sobreviveu ");
+ }
+}
+
